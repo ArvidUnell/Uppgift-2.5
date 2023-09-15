8806cbf410b0c831576885355e655a1577cd3346
arun06001 <arun06001@E-PW0234KE>
2023-09-15 10:39:36 +0200

    Add project files.

---
+++ Uppgift 2.5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Uppgift_2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en multiplikation");
+            string problem = Console.ReadLine();
+
+            byte multIndex = (byte)problem.IndexOf("*");
+            float tal1 = float.Parse(problem[..multIndex]);
+            float tal2 = float.Parse(problem[(multIndex + 1)..]);
+
+            Console.WriteLine($"={tal1*tal2}");
+            
+            Console.ReadKey();
+        }
+    }
+}
